02c2576fc288a5438f1bd08c4c33ab0ef5151bc5
Chilfish <chill4fish@gmail.com>
2023-10-12 22:23:21 +0800

    feat: /user

---
+++ src/main/kotlin/top/chilfish/chilpost/controller/UserController.kt
@@ -0,0 +1,32 @@
+package top.chilfish.chilpost.controller
+
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestAttribute
+import org.springframework.web.bind.annotation.RequestMapping
+import top.chilfish.chilpost.error.ErrorCode
+import top.chilfish.chilpost.error.newError
+import top.chilfish.chilpost.model.UserToken
+import top.chilfish.chilpost.service.UserService
+import top.chilfish.chilpost.utils.response
+
+@Controller
+@RequestMapping("/api/user")
+class UserController(
+    private val userService: UserService
+) {
+    @GetMapping("/me")
+    fun me(
+        @RequestAttribute("user") user: UserToken
+    ) = response(data = user)
+
+    @GetMapping("/@/{name}")
+    fun userHome(
+        @PathVariable name: String
+    ): Any {
+        val data = userService.userHome(name)
+            ?: throw newError(ErrorCode.NOT_FOUND_USER)
+        return response(data = data)
+    }
+}
---
+++ src/main/kotlin/top/chilfish/chilpost/service/UserService.kt
@@ -0,0 +1,36 @@
+package top.chilfish.chilpost.service
+
+import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
+import top.chilfish.chilpost.model.PostWithOwner
+import top.chilfish.chilpost.model.UserEntity
+import top.chilfish.chilpost.model.toPosts
+import top.chilfish.chilpost.model.toUser
+import top.chilfish.chilpost.utils.query
+
+@Service
+@Transactional
+class UserService {
+    fun getByName(name: String) = UserEntity.query(
+        "Select * From user_details As u " +
+                "Where u.name = '$name' " +
+                "And u.deleted = False;"
+    ).toUser().firstOrNull()
+
+    fun getPostByOwner(uid: Int) = PostWithOwner.query(
+        "Select * From post_with_owner As p " +
+                "Where owner_id = $uid " +
+                "  And is_body = True " +
+                "Order By created_at Desc;"
+    ).toPosts()
+
+    fun userHome(name: String): Map<String, Any>? {
+        val user = getByName(name) ?: return null
+        val posts = getPostByOwner(user.id)
+        return mapOf(
+            "user" to user,
+            "posts" to posts,
+            "count" to posts.size
+        )
+    }
+}
